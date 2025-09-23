7bcf2b31c666d137868883ff1bf7a023cb1e481e
Phakamas Chaichana <Phakasmas.c@ku.th>
2025-09-23 15:01:15 +0700

    .

--- assignments/week09/random_quiz-2.py
+++ assignments/week09/random_quiz-2.py
@@ -41,6 +41,8 @@ Example
 """
 
 import random
+random_number = random.randint(1, 100)
+print(random_number)
 
 def get_parity_hint(number):
     if number % 2 == 0:
@@ -57,9 +59,43 @@ def get_divisibility_hint(number):
         return "HINT: The number is NOT divisible by 3 or 5"
 
 def get_range_hint(number, current_min=1, current_max=100):
-    # Return narrowed range around the number
-    pass
+    lower = max(current_min, number - 10)
+    upper = min(current_max, number + 10)
+    return f"HINT: The number is between {lower} and {upper}"
 
 def get_thefirst_digit_hint(number):
-    # Retun the first digit of the number
-    pass
+    return f"HINT: The first digit of the number is {str(number)[0]}"
+
+attempt = 1
+
+print('=== Enhanced GUESSING GAME ===')
+print('Guess my number between 1 and 100!')
+print('You have unlimited attempts.\n')
+
+while True:
+    try:
+        guess_number = int(input(f'Attempt {attempt} - Enter your guess: '))
+        if guess_number <= 0:
+            print('Nunber must more than 0.')
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
+
+    if random_number == guess_number:
+        print('Congalutation.')
+        break
+    elif random_number > guess_number:
+        print(f'More than {guess_number}')
+    else:
+        print(f'Less than {guess_number}')
+
+    if attempt == 3:
+        print(get_parity_hint(random_number))
+    elif attempt == 5:
+        print(get_divisibility_hint(random_number))
+    elif attempt == 7:
+        print(get_range_hint(random_number))
+    elif attempt == 10:
+        print(get_thefirst_digit_hint(random_number))
+
+    attempt += 1
